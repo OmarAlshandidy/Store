c6c2a34a672b27f2031d24e6ed0a50637381348b
OmarAlshandidy <omaralshandiy@gmail.com>
2025-04-21 13:48:44 +0200

    Validation Error Handling

---
+++ Infrastructure/Presentation/BuggyController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BuggyController:ControllerBase
+    {
+        [HttpGet("notfound")] // Get : Api/Buggy/notfound
+        public IActionResult GetNotFoundRequest()
+        {
+            //code
+
+            return NotFound(); //404
+        }
+        [HttpGet("servererror")]  // Get : api/Buggy /servererror
+        public IActionResult GetServerErrorRequest()
+        {
+            throw new Exception();
+            return Ok();
+        }
+
+        [HttpGet("badrequest")]  // Get : api/Buggy /badrequest
+        public IActionResult GetbadRequest()
+        {
+            return BadRequest(); //400
+        }
+        [HttpGet("badrequest/{id}")]  // Get : api/Buggy /badrequest/id
+        public IActionResult GetbadRequest(int id ) // Validation Error
+        {
+            //Code
+            return BadRequest(); //400
+        }
+
+        [HttpGet("unauthorized")]  // Get : api/Buggy /unauthorized
+        public IActionResult GetUnauthorizedRequest()
+        {
+            //Code
+            return Unauthorized(); //401
+        }
+
+
+    }
+}
